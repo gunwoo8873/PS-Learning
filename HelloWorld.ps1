b170660c1552e07ee08faa289623987560d96a54
Choigunwoo <gunwoo8873@outlook.kr>
2024-11-04 12:51:52 +0900

    Update : Comment Waring Information

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -7,4 +7,4 @@ Write-Host "${VALUE}"
 # Case : "..." : 문자열의 변수가 보관이 된다. = ex. $VALUE = "String"
 #        '...' : 리터럴을 지정하여 값을 그대로 출력한다. = ex. $VALUE = 'String'
 #        $(..) : 개체의 속성을 보관이 된다. = ex. $($VALUE + Value)
-# Waring : 반드시 [#, Comment]는 스크립트에 있는 코드들과 같은 라인에 있어서는 안된다.
+# Waring : 권장하는 Comment 처리는 스크립트에 있는 코드들과 같은 라인에 있어서는 안된다.
